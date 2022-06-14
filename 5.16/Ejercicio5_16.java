a20d4cc62bdddc72d7b51c4d17fc0da09e4cca90
damarymejia <106458273+damarymejia@users.noreply.github.com>
2022-06-13 20:36:33 -0600

    Delete 5.16 directory

--- 5.16/Ejercicio5_16.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio5_16
-{        
-    public static void main( String args[])
-    { 
-        EjercicioPrueba5_16 miObjeto = new EjercicioPrueba5_16();
-
-        miObjeto.Imprime();
-    }
-} 
