785b2caf6c09d36756999892bf8df370c5d80eb7
damarymejia <106458273+damarymejia@users.noreply.github.com>
2022-06-13 20:36:54 -0600

    Delete 5.24 directory

--- 5.24/Ejercicio5_24.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio5_24 {
-    public static void main(String args[])
-    {     
-    EjercicioPrueba5_24 miObjeto = new EjercicioPrueba5_24();
-    miObjeto.Imprime();
-    }
-}
