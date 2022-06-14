67f1cc4f4645a1749aaa26f42d48b50b73efb278
damarymejia <106458273+damarymejia@users.noreply.github.com>
2022-06-13 20:37:03 -0600

    Delete 5.25 directory

--- 5.25/EjercicioPrueba5_25.java
+++
@@ -1,8 +0,0 @@
-public class EjercicioPrueba5_25 
-{     
-    public static void main(String args[])
-    { 
-        Ejercicio5_25 miObjeto = new Ejercicio5_25();
-        miObjeto.Imprime();
-    } 
-}
