501d7436871ae9990f5e3bef8eee6c55ce3c240c
damarymejia <106458273+damarymejia@users.noreply.github.com>
2022-06-13 20:23:50 -0600

    Add files via upload

---
+++ 4.18/EjercicioPrueba4_18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+ public class EjercicioPrueba4_18
+ 
+ { 
+    
+    public static void main(String args[])
+    { 
+    
+        Ejercicio4_18 miObjeto = new Ejercicio4_18();
+        miObjeto.Recibe();
+        
+    } 
+ } 
