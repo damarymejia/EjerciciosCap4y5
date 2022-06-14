28e2c84a61b2cc4740bdf92e25858e7287c0c22a
damarymejia <106458273+damarymejia@users.noreply.github.com>
2022-06-13 20:26:18 -0600

    Add files via upload

---
+++ 5.17/Ejercicio5_17.java
@@ -0,0 +1,8 @@
+public class Ejercicio5_17
+{
+    public static void main(String args[])
+    {   
+        EjercicioPrueba5_17 miObjeto = new EjercicioPrueba5_17();
+        miObjeto.Recibe(); 
+    }
+}
